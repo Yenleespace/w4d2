24701f00576fdffb7390220819efe727f36ad46b
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-11-30 11:17:59 -0500

    hello, added more into slideable (finished hor dir)

--- chess/slideable.rb
+++ chess/slideable.rb
@@ -7,20 +7,58 @@ module Slideable
         row_start = self.pos[0]
         col_start = self.pos[1]
 
-        (row_start...@board.length).each do |i|
+        (row_start + 1...board.length).each do |i|
             pos = [i, col_start]
             if self.board[i, col_start].is_a?(nullPiece)
-                HORIZONTAL_DIRS << [i, col_start] 
+                HORIZONTAL_DIRS << pos
             elsif self.color == self.board[i, col_start].color
                 break 
             elsif self.color != self.board[i, col_start].color
-                HORIZONTAL_DIRS << [i, col_start]
+                HORIZONTAL_DIRS << pos
                 break
             end
         end
 
+        (0...row_start).each do |i|
+            pos = [row_start - 1 - i, col_start]
+            if self.board[row_start - i - 1, col_start].is_a?(nullPiece)
+                HORIZONTAL_DIRS << pos
+            elsif self.color == self.board[row_start - i - 1, col_start].color
+                break 
+            elsif self.color != self.board[row_start - i - 1, col_start].color
+                HORIZONTAL_DIRS << pos
+                break
+            end
+        end
+
+        (col_start + 1...board.length).each do |i|
+            pos = [row_start, i]
+            if self.board[row_start, i].is_a?(nullPiece)
+                HORIZONTAL_DIRS << pos
+            elsif self.color == self.board[row_start, i].color
+                break 
+            elsif self.color != self.board[row_start, i].color
+                HORIZONTAL_DIRS << pos
+                break
+            end
+        end
+
+        (0...col_start).each do |i|
+            col = [col_start - 1 - i]
+            pos = [row_start, col]
+            if self.board[row_start, col].is_a?(nullPiece)
+                HORIZONTAL_DIRS << pos
+            elsif self.color == self.board[row_start, col].color
+                break 
+            elsif self.color != self.board[row_start, col].color
+                HORIZONTAL_DIRS << pos
+                break
+            end
+        end
 
     end
+
+    def valid
 
     def moves 
         HORIZONTAL_DIR + DIAGONAL_DIRS
