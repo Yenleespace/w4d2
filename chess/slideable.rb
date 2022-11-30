7b1e603d9ac9d7fa3ad71c82fe804272222a1f51
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-11-30 11:51:00 -0500

    more slideable work, moving over to Yen's computer

--- chess/slideable.rb
+++ chess/slideable.rb
@@ -3,15 +3,21 @@ module Slideable
     DIAGONAL_DIRS = []
 
     def horizontal_dirs
-        start_pos = self.pos
         row_start = self.pos[0]
         col_start = self.pos[1]
 
         (row_start + 1...board.length).each { |i|  moves(i, col_start, HORIZONTAL_DIRS) }
         (0...row_start).each { |i| moves(row_start - 1 - i, col_start, HORIZONTAL_DIRS) }
-        (col_start + 1...board.length).each { |i| moves(row_start, i, col_start) }
-        (0...col_start).each { |i| moves(row_start, col, HORIZONTAL_DIRS) }
+        (col_start + 1...board.length).each { |i| moves(row_start, i, HORIZONTAL_DIRS) }
+        (0...col_start).each { |i| moves(row_start, col_start - 1 - i, HORIZONTAL_DIRS) }
     end
+
+    def diagonal_dirs
+        row_start = self.pos[0]
+        col_start = self.pos[1]
+
+        col_start < row_start ? highest_val = row_start : highest_val = col_start
+        (highest_val + 1...board.length).each { |i| moves(row_start + i, col_start + i, DIAGONAL_DIRS) }
 
 
     def moves(row, col, arr)
