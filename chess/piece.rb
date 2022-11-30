aa350eff6f19de218f8b76800d131f7fd55697ac
Yen Lee <yenny0104@gmail.com>
2022-11-30 10:15:31 -0500

    slideable

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,8 +1,33 @@
 class Piece
-    def initialize(color, board, pos)
-        
-        
+    def initialize(color, board, pos=[])
+        @color = color
+        @board = board
+        @pos = pos
     end
+
+    #come back later ...
+    def to_s
+        # self.symbol.to_s 
+    end
+
+    def empty?
+        @pos.length == 0
+    end
+
+    def valid_moves
+    end
+
+    def pos=(val)
+        @pos = val
+    end
+
+    def color
+        @color
+    end
+
+    def move_into_check?(end_pos)
+    end
+
 
     
 end
